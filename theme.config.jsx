e98a9af7513740f267476fafd0f902bf71cee789
asimaranov <a.simaranov@gmail.com>
2023-11-25 13:12:33 +0300

    Initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,22 @@
+export default {
+    logo: <span>ZkNoid Project</span>,
+    project: {
+      link: 'https://github.com/aii23/MinaZKanoid'
+    },
+
+    footer: {
+        text: (
+          <span>
+            MIT {new Date().getFullYear()} ©{' '}
+            <a href="" target="_blank">
+              ZkNoid
+            </a>
+            .
+          </span>
+        )
+      }
+    
+    
+  }
+  
+  
