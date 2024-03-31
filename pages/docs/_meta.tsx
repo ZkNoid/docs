334bd95a1b14e253b6c5c16c83e906297fffbfd9
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-03-31 21:27:58 +0300

    Added initial info about cards sdk

--- pages/docs/_meta.tsx
+++ pages/docs/_meta.tsx
@@ -1,36 +1,38 @@
 export default {
-  "*": {
-    "title": "Documentation"
-  },
-  "index": "📙 Introduction",
-  "zk_gaming": "🎮 What is ZK gaming",
-  "competitions": "🥈 Competitions",
-  "-- Technical details": {
-    "type": "separator",
-    "title": "Technical details"
-  },
-  "network_architecture": "🕸 Network architecture",
-  "multiplayer": "👥 Multiplayer",
-  "arkanoid_details": {
-    "title": "🕹 Arkanoid game",
-  },
-  "randzu_details": {
-    "title": "🎴 Randzu game",
-  },
-  "thimblerig_details": {
-    "title": "🐚 Thimblerig game",
-  },
-  "-- For developers": {
-    "type": "separator",
-    "title": "For developers"
-  },
-  "sdk": "📦 SDK",
-  "game_building": "🧑‍💻 Building a simple game UI",
-  "research_grants": "📄 Research, develop grants",
-  "-- Tokenomics": {
-    "type": "separator",
-    "title": "Tokenomics"
-  },
-  "business_model": "💰 Revenue model",
-  "tokenomics": "🧩 Tokenomics",
-}
+    '*': {
+        title: 'Documentation',
+    },
+    index: '📙 Introduction',
+    zk_gaming: '🎮 What is ZK gaming',
+    competitions: '🥈 Competitions',
+    '-- Technical details': {
+        type: 'separator',
+        title: 'Technical details',
+    },
+    network_architecture: '🕸 Network architecture',
+    multiplayer: '👥 Multiplayer',
+    arkanoid_details: {
+        title: '🕹 Arkanoid game',
+    },
+    randzu_details: {
+        title: '🎴 Randzu game',
+    },
+    thimblerig_details: {
+        title: '🐚 Thimblerig game',
+    },
+    '-- For developers': {
+        type: 'separator',
+        title: 'For developers',
+    },
+    sdk: '📦 SDK',
+    game_building: '🧑‍💻 Building a simple game UI',
+    card_sdk: 'SDK for cards',
+    simple_card_game: 'Building simple card game',
+    research_grants: '📄 Research, develop grants',
+    '-- Tokenomics': {
+        type: 'separator',
+        title: 'Tokenomics',
+    },
+    business_model: '💰 Revenue model',
+    tokenomics: '🧩 Tokenomics',
+};
